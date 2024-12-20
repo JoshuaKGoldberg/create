9a4174d8897bc74a549bec7732e9845a768f51f4
Josh Goldberg <git@joshuakgoldberg.com>
2024-12-19 20:18:46 -0500

    Undid bin/index.js changes

--- packages/create/bin/index.js
+++ packages/create/bin/index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// import { runCli } from "../lib/cli/runCli.js";
+import { runCli } from "../lib/cli/runCli.js";
 
-console.log(process.argv);
-// process.exitCode = await runCli(...process.argv.slice(2));
+process.exitCode = await runCli(...process.argv.slice(2));
