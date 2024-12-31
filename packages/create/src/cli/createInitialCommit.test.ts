cee58d3b88dbc273f2ec050ce5c1fa8315a0fdf8
Josh Goldberg <git@joshuakgoldberg.com>
2024-12-31 00:09:22 -0500

    --amend test

--- packages/create/src/cli/createInitialCommit.test.ts
+++ packages/create/src/cli/createInitialCommit.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, test, vi } from "vitest";
 import { createInitialCommit } from "./createInitialCommit.js";
 
 describe("createInitialCommit", () => {
-	test("commands", async () => {
+	test("runs without --amend when amend is falsy", async () => {
 		const runner = vi.fn();
 
 		await createInitialCommit(runner);
@@ -15,6 +15,26 @@ describe("createInitialCommit", () => {
 			  ],
 			  [
 			    "git commit --message feat:\\ initialized\\ repo\\ ✨ --no-gpg-sign",
+			  ],
+			  [
+			    "git push -u origin main --force",
+			  ],
+			]
+		`);
+	});
+
+	test("runs with --amend when amend is true", async () => {
+		const runner = vi.fn();
+
+		await createInitialCommit(runner, true);
+
+		expect(runner.mock.calls).toMatchInlineSnapshot(`
+			[
+			  [
+			    "git add -A",
+			  ],
+			  [
+			    "git commit --message feat:\\ initialized\\ repo\\ ✨ --amend --no-gpg-sign",
 			  ],
 			  [
 			    "git push -u origin main --force",
