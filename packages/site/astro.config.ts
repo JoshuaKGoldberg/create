78fdabe8afb41a3024cca5f60ca296b64b14b6cf
Josh Goldberg <git@joshuakgoldberg.com>
2025-01-14 13:31:01 -0500

    docs: split site with starlight-sidebar-topics

--- packages/site/astro.config.ts
+++ packages/site/astro.config.ts
@@ -1,6 +1,7 @@
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 import { remarkHeadingId } from "remark-custom-heading-id";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 export default defineConfig({
 	integrations: [
@@ -14,56 +15,66 @@ export default defineConfig({
 			logo: {
 				src: "src/assets/favicon.png",
 			},
-			sidebar: [
-				{ label: "About", link: "about" },
-				{ label: "CLI", link: "cli" },
-				{ label: "Configuration", link: "configuration" },
-				{ label: "FAQs", link: "faqs" },
-				{ label: "Glossary", link: "glossary" },
-				{
-					collapsed: true,
-					items: [
-						{ label: "About", link: "engine/about" },
-						{
-							items: [
-								{ label: "Bases", link: "engine/concepts/bases" },
-								{ label: "Blocks", link: "engine/concepts/blocks" },
-								{ label: "Presets", link: "engine/concepts/presets" },
-								{ label: "Templates", link: "engine/concepts/templates" },
-							],
-							label: "Concepts",
-						},
-						{
-							items: [
-								{ label: "Contexts", link: "engine/runtime/contexts" },
-								{ label: "Creations", link: "engine/runtime/creations" },
-								{ label: "Execution", link: "engine/runtime/execution" },
-								{ label: "Inputs", link: "engine/runtime/inputs" },
-								{ label: "Merging", link: "engine/runtime/merging" },
-							],
-							label: "Runtime",
-						},
-						{
-							items: [
-								{ label: "Creators", link: "engine/apis/creators" },
-								{ label: "Producers", link: "engine/apis/producers" },
-								{ label: "Runners", link: "engine/apis/runners" },
-							],
-							label: "APIs",
-						},
-						{
-							items: [
-								{ label: "create-fs", link: "engine/packages/create-fs" },
-								{
-									label: "create-testers",
-									link: "engine/packages/create-testers",
-								},
-							],
-							label: "Packages",
-						},
-					],
-					label: "Templating Engine",
-				},
+			plugins: [
+				starlightSidebarTopics([
+					{
+						icon: "open-book",
+						items: [
+							{ label: "About", link: "about" },
+							{ label: "CLI", link: "cli" },
+							{ label: "Configuration", link: "configuration" },
+							{ label: "FAQs", link: "faqs" },
+							{ label: "Glossary", link: "glossary" },
+						],
+						label: "Get Started",
+						link: "/about",
+					},
+					{
+						icon: "information",
+						items: [
+							{ label: "About", link: "engine/about" },
+							{
+								items: [
+									{ label: "Bases", link: "engine/concepts/bases" },
+									{ label: "Blocks", link: "engine/concepts/blocks" },
+									{ label: "Presets", link: "engine/concepts/presets" },
+									{ label: "Templates", link: "engine/concepts/templates" },
+								],
+								label: "Concepts",
+							},
+							{
+								items: [
+									{ label: "Contexts", link: "engine/runtime/contexts" },
+									{ label: "Creations", link: "engine/runtime/creations" },
+									{ label: "Execution", link: "engine/runtime/execution" },
+									{ label: "Inputs", link: "engine/runtime/inputs" },
+									{ label: "Merging", link: "engine/runtime/merging" },
+								],
+								label: "Runtime",
+							},
+							{
+								items: [
+									{ label: "Creators", link: "engine/apis/creators" },
+									{ label: "Producers", link: "engine/apis/producers" },
+									{ label: "Runners", link: "engine/apis/runners" },
+								],
+								label: "APIs",
+							},
+							{
+								items: [
+									{ label: "create-fs", link: "engine/packages/create-fs" },
+									{
+										label: "create-testers",
+										link: "engine/packages/create-testers",
+									},
+								],
+								label: "Packages",
+							},
+						],
+						label: "Templating Engine",
+						link: "/engine/about",
+					},
+				]),
 			],
 			social: {
 				github: "https://github.com/JoshuaKGoldberg/create",
